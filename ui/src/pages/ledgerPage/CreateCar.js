c23a2504150e3e1e93b7a7182069d6bc942d0340
geonwoomun <ansejrrhkd@naver.com>
2019-10-28 19:49:52 +0900

    button 및 onSubmit

--- ui/src/pages/ledgerPage/CreateCar.js
+++ ui/src/pages/ledgerPage/CreateCar.js
@@ -25,12 +25,13 @@ const HomepageLayout = () => {
     }
     return (
         <div>
-            <form>
+            <form onSubmit={onSubmit}>
                 <label>carno</label><input style = {{display: "block"}} value = {carno} onChange={e=> setCarno(e.target.value)}/>
                 <label>colour</label><input style = {{display: "block"}} value = {colour} onChange={e=> setColour(e.target.value)}/>
                 <label>make</label><input style = {{display: "block"}} value = {make} onChange={e=> setMake(e.target.value)}/>
                 <label>model</label><input style = {{display: "block"}} value = {model} onChange={e=> setModel(e.target.value)}/>
                 <label>owner</label><input style = {{display: "block"}} value = {owner} onChange={e=> setOwner(e.target.value)}/>
+                <button>생성</button>
             </form>        
         </div>
     );
