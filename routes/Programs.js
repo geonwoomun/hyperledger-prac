9715e67db5d13a4ad760a672357cb0d8a20f4bbc
geonwoomun <ansejrrhkd@naver.com>
2019-11-08 17:53:31 +0900

    에러수정

--- routes/Programs.js
+++ routes/Programs.js
@@ -55,7 +55,7 @@ Programs.post('/register', async (req, res) => {  // 프로그램 등록
         program
     )
     .then(async (results) => {
-    const proNum = results.proNum
+    const proNum = ""+results.proNum 
 
     const walletPath = path.join(process.cwd(),"..", "wallet");
 
